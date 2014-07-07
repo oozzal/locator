32447a2573983b2d6e647b189274d2274ac756d5
oozzal <theoozzal@gmail.com>
2014-07-07 07:38:38 +0545

    Add Bluetooth toggle and discovery.

--- www/cordova_plugins.js
+++ www/cordova_plugins.js
@@ -1,7 +1,17 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "../plugins/com.phonegap.plugins.bluetooth/www/bluetooth.js",
+        "id": "com.phonegap.plugins.bluetooth.bluetooth",
+        "clobbers": [
+            "bluetooth"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "com.phonegap.plugins.bluetooth": "0.9"
+}
 // BOTTOM OF METADATA
 });
--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,5 @@
-// var bluetoothSerial = cordova.require('bluetoothSerial');
-
 var app = {
+  isBluetoothEnabled: false,
   // Application Constructor
   initialize: function() {
     this.bindEvents();
@@ -11,25 +10,66 @@ var app = {
   // Bind any events that are required on startup. Common events are:
   // 'load', 'deviceready', 'offline', and 'online'.
   bindEvents: function() {
-    $(document).on('deviceready', this.onDeviceReady);
-    $(document).on('bluetoothready', this.onBluetoothReady);
+    $(document).on("deviceready", this.onDeviceReady);
+    $(document).on("bluetoothon", this.onBluetoothOn);
+    $(document).on("bluetoothoff", this.onBluetoothOff);
+    $("#bluetooth #enable-bluetooth").on("click", this.enableBluetooth);
+    $("#bluetooth #disable-bluetooth").on("click", this.disableBluetooth);
+    $("#bluetooth #start-discovery").on("click", this.startDiscovery);
   },
   // Check if necessary hardwares/settings are present
   setup: function() {
-    if(true) $(document).trigger('bluetoothready');
+    typeof window.bluetooth !== "undefined" && window.bluetooth.isEnabled(function(isEnabled) {
+      (isEnabled && $(document).trigger("bluetoothon")) || $(document).trigger("bluetoothoff");
+    })
   },
   // deviceready Event Handler
   //
   // The scope of 'this' is the event. In order to call the 'receivedEvent'
   // function, we must explicitly call 'app.receivedEvent(...);'
   onDeviceReady: function() {
-    $("#deviceready>.listening").css("display", "none");
-    $("#deviceready>.received").css("display", "block");
+    $("#deviceready .listening").hide();
+    $("#deviceready .received").show();
   },
   // after bluetooth is ready
-  onBluetoothReady: function() {
-    $("#bluetoothready>.listening").css("display", "none");
-    $("#bluetoothready>.received").css("display", "block");
+  onBluetoothOn: function() {
+    $("#bluetooth #bluetoothready .listening").hide();
+    $("#bluetooth #bluetoothready .received").show();
+    $("#bluetooth #discovery").show();
+    $("#bluetooth #enable-bluetooth").hide();
+    $("#bluetooth #disable-bluetooth").show();
+    this.isBluetoothEnabled = true;
+  },
+  // after bluetooth is disabled
+  onBluetoothOff: function() {
+    $("#bluetooth #bluetoothready .received").hide();
+    $("#bluetooth #bluetoothready .listening").show();
+    $("#bluetooth #discovery").hide();
+    $("#bluetooth #enable-bluetooth").show();
+    $("#bluetooth #disable-bluetooth").hide();
+    this.isBluetoothEnabled = false;
+  },
+  // enable bluetooth
+  enableBluetooth: function() {
+    window.bluetooth.enable(function() {
+      $(document).trigger("bluetoothon");
+    });
+  },
+  // disable bluetooth
+  disableBluetooth: function() {
+    window.bluetooth.disable(function() {
+      $(document).trigger("bluetoothoff");
+    });
+  },
+  // start devices discovery
+  startDiscovery: function() {
+    alert("discovery started" + app.isBluetothEnabled);
+    window.bluetooth.startDiscovery(
+      function(device) {
+        $("#bluetooth #devices-list").append(device.name + " => " + device.address + "<br/ ");
+      },
+      function() { alert("done discovery"); }
+    );
   }
 };
 
